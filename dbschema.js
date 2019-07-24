346c3cace76c43c53f48adf3afefdc222addf021
Emmy <gmemmy22@gmail.com>
2019-07-24 02:38:51 +0100

    Integrate validation for sign up route

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  posts: [
+    {
+      username: "user",
+      body: "This is the post body",
+      createdAt: "2019-07-23T14:06:57.113Z",
+      likeCount: 5,
+      commentCount: 5
+    }
+  ]
+};
