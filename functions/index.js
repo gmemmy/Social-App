23e234e1153dc35538587ddfb4e2d1b2c165f967
Emmy <gmemmy22@gmail.com>
2019-07-24 02:36:14 +0100

    Integrate validation for sign up route

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-escape */
 /* eslint-disable promise/always-return */
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
@@ -57,6 +58,18 @@ app.post("/post", (req, res) => {
     });
 });
 
+const isEmail = email => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) return true;
+  else return false;
+};
+
+const isEmpty = string => {
+  if (string.trim() === "") {
+    return true;
+  } else return false;
+};
+
 // Signup route
 app.post("/signup", (req, res) => {
   const { email, password, confirmPassword, username } = req.body;
@@ -67,7 +80,29 @@ app.post("/signup", (req, res) => {
     username: username
   };
 
+  let errors = {};
+
+  // Auth validation
+  if (isEmpty(newUser.email)) {
+    errors.email = "Must not be empty";
+  } else if (!isEmail(newUser.email)) {
+    errors.email = "Must be a valid email address";
+  }
+
+  if (isEmpty(newUser.password)) {
+    errors.password = "Must not be empty";
+  }
+  if (newUser.password !== newUser.confirmPassword) {
+    errors.confirmPassword = "Passwords must match";
+  }
+  if (isEmpty(newUser.username)) {
+    errors.username = "Must not be empty";
+  }
+
+  if (Object.keys(errors).length > 0) return res.status(400).json({ errors });
+
   // TODO validate data
+  let token, UserId;
   db.doc(`/users/${newUser.username}`)
     .get()
     .then(doc => {
@@ -82,18 +117,29 @@ app.post("/signup", (req, res) => {
       }
     })
     .then(data => {
+      UserId = data.user.uid;
       return data.user.getIdToken();
     })
-    .then(token => {
+    .then(idToken => {
+      token = idToken;
+      const userCredentials = {
+        username: newUser.username,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        UserId
+      };
+      return db.doc(`/users/${newUser.username}`).set(userCredentials);
+    })
+    .then(() => {
       return res.status(201).json({ token });
     })
     .catch(err => {
       console.error(err);
-      if (err.code === 'auth/email-already-in-use') {
-        return res.status(400).json({ email: 'Email is already in use' })
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email is already in use" });
       } else {
-        return res.status(500).json({ error: err.code }); 
-      }   
+        return res.status(500).json({ error: err.code });
+      }
     });
 });
 
