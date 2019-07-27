4da5cbf44f2fe295b03e82ad3cb75e0b3694b7d5
Emmy <gmemmy22@gmail.com>
2019-07-27 02:05:20 +0100

    add functionality that updates commentCount property whenever a new comment has been made on a post

--- functions/handlers/posts.js
+++ functions/handlers/posts.js
@@ -72,6 +72,9 @@ exports.commentOnPost = (req, res) => {
       if (!doc.exists) {
         return res.status(400).json({ error: "Post not found" });
       }
+      return doc.ref.update({ commentCount: doc.data().commentCount + 1 });
+    })
+    .then(() => {
       return db.collection("comments").add(newComment);
     })
     .then(() => {
